095067909eed69c56650ae776d9a07683a5f9aff
SlavicMag <govoruxadaniil@gmail.com>
2026-01-22 20:08:48 +0300

    Изменил скрипт Fade опять)
    Так как код с математической сменой непрозрачности кнопок оказался сильное негативное влияние на оптимизацию и моей душевной близости к анимациям, было принято решение вновь использовать их, заместо изменения непрозрачности через код. Задача была выполнена с помощью компонента canvas group.

--- PlanetCreator/Assets/Scripts/Animations/Fade.cs
+++ PlanetCreator/Assets/Scripts/Animations/Fade.cs
@@ -1,54 +1,20 @@
 using UnityEngine;
-using UnityEngine.UI;
-using TMPro;
 
 public class Fade : MonoBehaviour
 {
-    [SerializeField] private GameObject[] m_buttons;
-    [SerializeField] private float fadeSpeed = 2f;
+    [SerializeField] private Animator m_canvasAnimator;
+    [SerializeField] private CanvasGroup m_buttons;
 
-    private float targetAlpha = 1f; // значение к которому стремится текущая непрозрачность
-    private float currentAlpha = 1f; // текущая непрозрачность
-
-    public void FadeIn() => targetAlpha = 1f;
-    public void FadeOut() => targetAlpha = 0f;
-
-    // чекаем есть ли разница между currentAlpha и targetAlpha
-    private void Update()
+    public void FadeIn()
     {
-        if (Mathf.Abs(currentAlpha - targetAlpha) > 0.001f)
-        {
-            // плавно меняем нашу непрозрачность без резких скачков
-            currentAlpha = Mathf.MoveTowards(currentAlpha, targetAlpha, Time.deltaTime * fadeSpeed);
-            ApplyAlphaToAll(currentAlpha);
-        }
+        m_buttons.blocksRaycasts = true;
+        m_canvasAnimator.SetBool("Fade", true);
     }
 
-    private void ApplyAlphaToAll(float alpha)
+    public void FadeOut()
     {
-        // устанавливаем рамки от 0 до 1
-        alpha = Mathf.Clamp01(alpha);
-
-        // проходимся по всем объектам массива
-        foreach (var btn in m_buttons)
-        {
-            // проверка наличия элемента 
-            if (!btn) continue;
-
-            // (1) попытка получения указанного компонента и в случае успеха сохраняем в img
-            if (btn.TryGetComponent<Image>(out var img))
-                // смена прозрачности
-                img.color = new Color(img.color.r, img.color.g, img.color.b, alpha);
-
-            // поиск компонента среди род. и доч. объектов и сохранение в text, если результат не null
-            if (btn.GetComponentInChildren<TextMeshProUGUI>() is TextMeshProUGUI text)
-                // смена прзрачности текста
-                text.color = new Color(text.color.r, text.color.g, text.color.b, alpha);
-
-            // аналогичноо (1)
-            if (btn.TryGetComponent<Button>(out var button))
-                // активность кнопки, но хз подойдёт ли такое значения порога её активности?
-                button.interactable = alpha > 0.6f;
-        }
+        m_buttons.blocksRaycasts = false;
+        //Debug.Log("Ауч!");
+        m_canvasAnimator.SetBool("Fade", false);
     }
 }
