291f77e9c3b585add9bbaff4bf99ef8d10a7859c
YKJ <vickykat2468@gmail.com>
2026-01-26 23:25:45 +0300

    Rework Planet SO
    + add List of possible chemical combinations

---
+++ PlanetCreator/Assets/Scripts/Planets/ChemicalCombination.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using UnityEngine;
+
+namespace Planets
+{
+    [Serializable]
+    public class ChemicalCombination
+    {
+        [SerializeField] private ChemicalType[] m_chemicalTypes;
+    }
+}
--- PlanetCreator/Assets/Scripts/Planets/FinalPlanet.cs
+++ PlanetCreator/Assets/Scripts/Planets/FinalPlanet.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace Planets
@@ -5,7 +6,7 @@ namespace Planets
     [CreateAssetMenu(menuName = "Planet")]
     public class FinalPlanet : ScriptableObject
     {
-        [SerializeField] private ChemicalType[] m_chemical = new ChemicalType[3];
+        [SerializeField] private List<ChemicalCombination> m_combinations;
         [SerializeField] private Remoteness m_remoteness;
         [SerializeField] private Mass m_mass;
         [SerializeField] private Size m_size;
