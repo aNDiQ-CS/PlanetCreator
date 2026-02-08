adf1b7a2e39e344604bbc5ff1a5f57feaa56a277
SlavicMag <govoruxadaniil@gmail.com>
2026-02-08 22:10:47 +0300

    Модернизировал Fade кнопки
    Модернизировал Fade кнопки чтобы использовать их как префаб, также превратил pop-ap кнопки в префаб

--- PlanetCreator/Assets/Scripts/Animations/UI/Fade.cs
+++ PlanetCreator/Assets/Scripts/Animations/UI/Fade.cs
@@ -2,18 +2,20 @@ using UnityEngine;
 
 public class Fade : MonoBehaviour
 {
-    [SerializeField] private Animator m_canvasAnimator;
+    [SerializeField] private Animator m_buttonAnimator;
     [SerializeField] private CanvasGroup m_buttons;
 
     public void FadeIn()
     {
         m_buttons.blocksRaycasts = true;
-        m_canvasAnimator.SetBool("Fade", true);
+        m_buttons.interactable = true;
+        m_buttonAnimator.SetBool("Fade", true);
     }
 
     public void FadeOut()
     {
         m_buttons.blocksRaycasts = false;
-        m_canvasAnimator.SetBool("Fade", false);
+        m_buttons.interactable = false;
+        m_buttonAnimator.SetBool("Fade", false);
     }
 }
